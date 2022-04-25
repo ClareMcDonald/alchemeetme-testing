8494542e6e430d06be9371e61c81d9e10fb23a9d
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-04-25 15:26:19 -0700

    test for header image passes

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,29 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Header from './components/layout/Header';
+import App from './App';
 
-test('Should render the header', () => {
+const user = {
+    id: 1,
+    created_at: '2021-12-13T00:17:29+00:00',
+    name: 'Vonta',
+    avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
+    header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
+    likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
+    motto: 'Res Non Verba',
+    color: 'crimson',
+}
+  
+describe('header', () => {
+    test('Should render the header', async () => {
+        render(
+            <MemoryRouter>
+                <Header user={user}/>
+            </MemoryRouter>
+        );
 
-})
+        const alt = screen.getByAltText('Alchemy Logo');
+
+        expect(alt).toBeInTheDocument();
+    });   
+});
